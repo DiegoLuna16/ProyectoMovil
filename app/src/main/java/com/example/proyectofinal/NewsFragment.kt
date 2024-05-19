aaf313c729140cfbf626f2f660aa0dd316e73382
Diego Rocha <drochaesc@gmail.com>
2024-05-19 16:33:01 -0600

    Agregando video a Fragmento de noticias

--- app/src/main/java/com/example/proyectofinal/NewsFragment.kt
+++ app/src/main/java/com/example/proyectofinal/NewsFragment.kt
@@ -1,12 +1,19 @@
 package com.example.proyectofinal
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
 
 class NewsFragment : Fragment() {
+
+    private var myVideoView : VideoView? = null
+    private var myMediaController: MediaController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +21,40 @@ class NewsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        myVideoView = view.findViewById(R.id.videoView)
+
+        setUpVideoPlayer()
+    }
+
+    private fun setUpVideoPlayer() {
+        if (myMediaController == null) {
+            myMediaController = MediaController(context)
+            myMediaController!!.setAnchorView(myVideoView)
+        }
+
+        myVideoView!!.setMediaController(myMediaController)
+        myVideoView!!.setVideoURI(
+            Uri.parse(
+                "android.resource://" + requireActivity().packageName + "/" + R.raw.anuncio
+            )
+        )
+
+        myVideoView!!.requestFocus()
+
+        myVideoView!!.start() // Puedes iniciar el video automáticamente si deseas
+
+        myVideoView!!.setOnCompletionListener {
+            Toast.makeText(context, "Video Completed", Toast.LENGTH_SHORT).show()
+        }
+
+        myVideoView!!.setOnErrorListener { mp, what, extra ->
+            Toast.makeText(context, "An Error Occurred", Toast.LENGTH_SHORT).show()
+            false
+        }
     }
 }
