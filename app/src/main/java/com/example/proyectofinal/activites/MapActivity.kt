c64c7660e586c10afbee7cbc4ab1f5e4ad556661
Diego Rocha <drochaesc@gmail.com>
2024-05-21 12:50:37 -0600

    Agregando localización en mapa a recycler view de localizaciones

---
+++ app/src/main/java/com/example/proyectofinal/activites/MapActivity.kt
@@ -0,0 +1,95 @@
+package com.example.proyectofinal.activites
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectofinal.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback,
+    GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    private lateinit var txtLatitud: EditText
+    private lateinit var txtLongitud: EditText
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+        txtLatitud = findViewById(R.id.txtLatitud)
+        txtLongitud = findViewById(R.id.txtLongitud)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapa)
+                as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
+        val locationName = intent.getStringExtra("locationName")
+        val latitude = intent.getDoubleExtra("latitud", 0.0)
+        val longitude = intent.getDoubleExtra("longitud", 0.0)
+
+        // Asignar los valores a los EditText
+        txtLatitud.setText(latitude.toString())
+        txtLongitud.setText(longitude.toString())
+
+        val textViewLocationName = findViewById<TextView>(R.id.textViewLocationName)
+        textViewLocationName.text = locationName
+
+        setup();
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.setOnMapClickListener(this)
+        mMap.setOnMapLongClickListener(this)
+
+        // Agregar marcador en la ubicación especificada
+        addMarkerAtLocation(LatLng(txtLatitud.text.toString().toDouble(), txtLongitud.text.toString().toDouble()))
+    }
+
+    private fun addMarkerAtLocation(location: LatLng) {
+        mMap.clear() // Limpiar marcadores anteriores
+        mMap.addMarker(MarkerOptions().position(location).title("Marcador"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+    }
+
+    override fun onMapClick(latLng: LatLng) {
+        txtLatitud.setText(latLng.latitude.toString())
+        txtLongitud.setText(latLng.longitude.toString())
+        mMap.clear()
+        val mexico = LatLng(latLng.latitude, latLng.longitude)
+        mMap.addMarker(MarkerOptions().position(mexico).title(""))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico))
+    }
+
+    override fun onMapLongClick(latLng: LatLng) {
+        txtLatitud.setText(latLng.latitude.toString())
+        txtLongitud.setText(latLng.longitude.toString())
+        mMap.clear()
+        val mexico = LatLng(latLng.latitude, latLng.longitude)
+        mMap.addMarker(MarkerOptions().position(mexico).title(""))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico))
+    }
+
+    private fun setup() {
+        val btnReturn: MaterialButton = findViewById(R.id.btnHome)
+
+        btnReturn.setOnClickListener {
+            showMain()
+        }
+    }
+
+    private fun showMain() {
+        val mainIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainIntent)
+    }
+
+}
